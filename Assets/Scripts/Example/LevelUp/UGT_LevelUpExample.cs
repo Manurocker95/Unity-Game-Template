e1fae397702a9cc784a284ebb84bebed3f1d1c3e
Manu <manurocker95@gmail.com>
2018-11-13 13:12:30 +0100

    Level up example

---
+++ Assets/Scripts/Example/LevelUp/UGT_LevelUpExample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameTemplate.Game;
+
+namespace UnityGameTemplate.Example
+{
+    public class UGT_LevelUpExample : UGT_Example
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        protected override void Update()
+        {
+
+            if (UGT_InputManager.IsKeyCodePressedDown(KeyCode.L))
+            {
+                UGT_GameManager.Instance.Player.LevelUp();
+            }
+
+            base.Update();
+        }
+    }
+
+}
